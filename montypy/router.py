a69ad150cfc6664f86083a9e213e779c594cd45a
George Violaris <violarisgeorge@gmail.com>
2014-04-23 16:30:44 +0300

    Added basic system info retrieval

--- montypy/router.py
+++ montypy/router.py
@@ -1,5 +1,8 @@
 import cherrypy
 import os
+import platform
+import socket
+import json
 
 MEDIA_DIR = os.path.join(os.path.abspath("."), u"web")
 
@@ -17,9 +20,18 @@ class LoginPage(object):
 
     @cherrypy.expose
     def request(self, username, password):
-        print "Debug"
+        #print "Debug"
         return password
-
+    
+    @cherrypy.expose
+    def sysinfo(self):
+        sysinfo = [platform.system(),platform.release(),platform.version(),socket.gethostname()]
+        return json.dumps(  [{  
+                                "System"    : sysinfo[0], 
+                                "Release"   : sysinfo[1], 
+                                "Version"   : sysinfo[2],
+                                "Hostname"  : sysinfo[3]
+                            }])
 
 config = {'/web': {'tools.staticdir.on': True, 'tools.staticdir.dir': MEDIA_DIR, }}
 
